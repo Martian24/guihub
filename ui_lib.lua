7f68d78d44ed24709f666a78eca502c4abc0afc8
Martian24 <49285034+Martian24@users.noreply.github.com>
2021-08-10 02:46:21 +0200

    Update ui_lib.lua

--- ui_lib.lua
+++ ui_lib.lua
@@ -546,6 +546,7 @@ function library.init(self)
         section.ScaleType = Enum.ScaleType.Slice
         section.SliceCenter = Rect.new(100,100,100,100)
         section.SliceScale = 0.06
+        section.ClipsDescendants = false
         section.Parent = window
 
         title.Name = "title"
